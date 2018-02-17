35b5ed5d98abdb0ec221b239bb1e67cf63d7e22b
NATTADET CHINTHANATHATSET <nattadet.chi@student.mahidol.ac.th>
2018-02-18 00:00:32 +0700

    second test

--- sqltest.js
+++ sqltest.js
@@ -1,17 +1,37 @@
+const mysql = require('mysql2');
+
+var config =
+{
+    host: 'begeekdb.mysql.database.azure.com',
+    user: 'adminrickky@begeekdb',
+    password: 'Admin1234',
+    database: 'mydb',
+    port: 3306,
+    ssl: true
+};
+const conn = new mysql.createConnection(config);
+
+conn.connect(
+   function (err){
+        if (err) {
+            console.log("!!! Cannot connect !!! Error:");
+            throw err;
+        }
+        else {
+            console.log("Connected!");
+            readData();
+        }
+    });
 
 function readData(){
-        conn.query('SELECT * FROM pills_db',
+        conn.query('SELECT * FROM mydb.pills_db',
             function (err, results, fields) {
                 if (err) throw err;
-                else console.log('Selected ' + results.length + ' row(s).');
-                for (i = 0; i < results.length; i++) {
-                    console.log('Row: ' + JSON.stringify(results[i]));
+                else console.log('Selected ' + results + ' row(s).');
+                for (i = 0; i < 20; i++) {
+                    console.log('Row: '+i+ JSON.stringify(results[i]));
                 }
                 console.log('Done.');
             })
-       conn.end(
-           function (err) {
-                if (err) throw err;
-                else  console.log('Closing connection.')
-        });
+
 };
